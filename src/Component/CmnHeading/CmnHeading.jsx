17c79edb63fee0569a20647ad83d065e0ddefc03
kikstart2026-dev <kikstart2026@gmail.com>
2026-02-16 21:59:36 +0530

    add paragraph in cmnHeading

--- src/Component/CmnHeading/CmnHeading.jsx
+++ src/Component/CmnHeading/CmnHeading.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./CmnHeading.scss";
 
-export default function CmnHeading({ title, subtitle, align = "center" }) {
+export default function CmnHeading({ title, subtitle, details, align = "center" }) {
   return (
     <div className={`common-heading text-${align}`}>
         <h2>{title}</h2>
         <h3>{subtitle}</h3>
+        <p>{details}</p>
     </div>
   )
 }
