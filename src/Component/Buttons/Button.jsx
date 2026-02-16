5b457050817c3f88a4bf2844bab3fce59faf7e62
kikstart2026-dev <kikstart2026@gmail.com>
2026-02-16 13:32:35 +0530

    Add components Buttons and a Main.scss file

---
+++ src/Component/Buttons/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Button.scss";
+
+export default function Button({ text, onClick, variant = "primary" }) {
+  return (
+    <button className={`custom-btn ${variant}`} onClick={onClick}>
+      <span>{text}</span>
+    </button>
+  );
+}
+
