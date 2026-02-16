b7126018f194a5ae95fb270dfc81f4294f61df1e
kikstart2026-dev <kikstart2026@gmail.com>
2026-02-16 20:59:17 +0530

    Center common heading added

---
+++ src/Component/CmnHeading/CmnHeading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./CmnHeading.scss";
+export default function CmnHeading() {
+  return (
+    <div className='why-choose-us'>
+        <h2>Why Choose Us</h2>
+        <h3>Give the Gift of Gym</h3>
+    </div>
+  )
+}
