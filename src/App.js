743f96def0b77567baa236b4c73666c20e672c8e
kikstart2026-dev <kikstart2026@gmail.com>
2026-02-18 01:04:53 +0530

    rename shared folder to Shared

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from './Pages/Home/Home';
-import Header from './shared/Header/Header';
+import Header from './Shared/Header/Header';
 
 
 
