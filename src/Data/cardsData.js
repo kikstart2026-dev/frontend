5ee482a9d7cdc361a1a07310c305e8518a4df15c
kikstart2026-dev <kikstart2026@gmail.com>
2026-02-18 19:03:13 +0530

    temp rename

---
+++ src/Data/cardsData.js
@@ -0,0 +1,31 @@
+import coach from "../assets/images/human_logo.png";
+import work from "../assets/images/work.png";
+import safety from "../assets/images/safety.png";
+import love from "../assets/images/love.png";
+
+export const CardsData = [
+  {
+    icon: coach,
+    title: "Experienced Coaches",
+    description: "Lorem ipsum dolor sit amet consectetur. Nunc id adipiscing at interdum eu viverra.",
+    color: "#FFF8F8"
+  },
+  {
+    icon: work,
+    title: "Work Experience",
+    description: "Lorem ipsum dolor sit amet consectetur. Nunc id adipiscing at interdum eu viverra.",
+    color: "#FFFBEF"
+  },
+  {
+    icon: safety,
+    title: "Care And Safety",
+    description: "Lorem ipsum dolor sit amet consectetur. Nunc id adipiscing at interdum eu viverra.",
+    color: "#EFF7FD"
+  },
+  {
+    icon: love,
+    title: "Love For Children",
+    description: "Lorem ipsum dolor sit amet consectetur. Nunc id adipiscing at interdum eu viverra.",
+    color: "#FFF8F1"
+  }
+];
