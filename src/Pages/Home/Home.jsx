0ef408fa3f863368702c8db9a2bc796c6434c684
kikstart2026-dev <kikstart2026@gmail.com>
2026-02-19 03:41:39 +0530

    My final code

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,19 +5,12 @@ import "../../Main.scss";
 import MiniCardSection from "../../Component/WhyChooseUs/MiniCardSection/MiniCardSection";
 import FaqSection from "../../Component/Faqs/Faqs-Section/FaqSection";
 import HomeBanner from "../../Component/HomeBanner/HomeBanner";
-import TwoSide from "../../Component/SectionTwoSIde/TwoSide/TwoSide";
-import TestSection from "../../Component/Testimonial/Test/TestSection";
 
 export default function Home() {
     return (
         <>
             <section className="Home-Banner">
                 <HomeBanner />
-            </section>
-            <section className="about-us">
-                <div className="container">
-                    <TwoSide/>
-                </div>
             </section>
             <section className="MiniCard">
                 <div className="container">
@@ -27,12 +20,6 @@ export default function Home() {
             
             <section className="program-part">
                 <ProgramsSection />
-            </section>
-            <section className="Testimonial">
-                <div className="container">
-                    <TestSection />
-                </div>
-                
             </section>
             <section className="Faqs-section">
                 <div className="container">
