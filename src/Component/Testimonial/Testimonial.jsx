f24418748a8113e35a22524b5b526a791c7c9734
kikstart2026-dev <kikstart2026@gmail.com>
2026-02-19 02:49:37 +0530

    rupak complete

--- src/Component/Testimonial/Testimonial.jsx
+++ src/Component/Testimonial/Testimonial.jsx
@@ -6,7 +6,7 @@ export default function Testimonial({ img, para, miniheading, coach }) {
     return (
         <div>
             
-            <figure className='girl'><img  src={img} /></figure>
+            <figure className='girl2'><img  src={img} /></figure>
             <p className='para same'>{para}</p>
             <h3 className='miniheading same'>{miniheading}</h3>
             <h4 className='coach same'>{coach}</h4>
