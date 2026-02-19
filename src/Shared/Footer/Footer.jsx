60ab8f4ca67c2afc9278857972224fb34ef0dcb3
kikstart2026-dev <kikstart2026@gmail.com>
2026-02-19 18:23:14 +0530

    correct footer & banner with responsive

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
-import sms from "../../assets/images/sms.png"
+import sms from "../../assets/images/sms.png";
 import "./Footer.scss";
+
+const quickLinks = [
+  { label: "About Us", path: "/about" },
+  { label: "Programs", path: "/programs" },
+  { label: "Contact Us", path: "/contact" },
+  { label: "Faqs", path: "/faqs" },
+];
+
+const legalLinks = [
+  { label: "Terms and Condition", path: "/terms" },
+  { label: "Privacy Policy", path: "/privacy" },
+];
 
 export default function Footer() {
   return (
@@ -10,78 +22,55 @@ export default function Footer() {
       <footer>
         <div className="container">
           <div className="total">
-
+            {/* First */}
             <div className="first">
-              <img src={logo} alt="" />
-              <p>Lorem ipsum dolor sit amet consectetur. Nunc id adipiscing at interdum eu viverra. </p>
+              <Link to="/"> <img src={logo} alt="Logo" /> </Link>
+              <p>Lorem ipsum dolor sit amet consectetur. Nunc id adipiscing at interdum eu viverra.</p>
               <div className="sms">
-                <img src={sms} alt=""/>
+                <img src={sms} alt="Email Icon" />
                 <p>info@KikStartKids.com</p>
               </div>
             </div>
 
+            {/* Quick Links */}
             <div className="second">
               <h5>Quick links</h5>
               <ul>
-                <li>
-                  <Link to="/about">
-                    About Us
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/programs">
-                    Programs
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/contact">
-                    Contact Us
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/faqs">
-                    Faqs
-                  </Link>
-                </li>
+                {quickLinks.map((link, idx) => (
+                  <li key={idx}>
+                    <Link to={link.path}>{link.label}</Link>
+                  </li>
+                ))}
               </ul>
             </div>
 
+            {/* Legal Links */}
             <div className="third">
               <h5>Legal</h5>
               <ul>
-                <li>
-                  <Link to="/terms">
-                    Terms and Condition
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/privacy">
-                    Privacy Policy
-                  </Link>
-                </li>
+                {legalLinks.map((link, idx) => (
+                  <li key={idx}>
+                    <Link to={link.path}>{link.label}</Link>
+                  </li>
+                ))}
               </ul>
             </div>
 
+            {/* Newsletter */}
             <div className="fourth">
               <h5>Newsletter</h5>
               <p>Enter the email to subscribe our newsletter</p>
-
               <div className="search">
                 <input
                   type="text"
                   placeholder="Enter Email"
                   className="search-input"
                 />
-                <button className="submit-btn" type="submit" >
+                <button className="submit-btn" type="submit">
                   <span>→</span>
                 </button>
               </div>
             </div>
-
           </div>
         </div>
       </footer>
@@ -89,10 +78,10 @@ export default function Footer() {
       <div className="container">
         <div className="footer-lower">
           <p>© Copyright 2024 kikstart - All Rights Reserved</p>
-          <p className='right'>Website Design by Webskitters</p>
+          <p className="right">Website Design by Webskitters</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
