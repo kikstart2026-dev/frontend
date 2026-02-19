b087599183484113b423cf088203bf4a8441d9d6
kikstart2026-dev <kikstart2026@gmail.com>
2026-02-19 18:51:11 +0530

    Home page done with bootstrap

--- src/Component/Services/ProgramsSection/ProgramsSection.jsx
+++ src/Component/Services/ProgramsSection/ProgramsSection.jsx
@@ -6,38 +6,40 @@ import Button from "../../Buttons/Button";
 import { Link } from "react-router-dom";
 import CmnHeading from "../../CmnHeading/CmnHeading";
 
-
 export default function ProgramsSection() {
   return (
-    <>
-      <section className="back-color">
-        <div className="container">
-          <div className="why-choose-us">
-            <CmnHeading
-              title="SERVICES"
-              subtitle="Children’s Fitness Programs"
-              align="center"
-            />
-          </div>
+    <section className="back-color">
+      <div className="container">
+        <div className="why-choose-us">
+          <CmnHeading
+            title="SERVICES"
+            subtitle="Children’s Fitness Programs"
+            align="center"
+          />
+        </div>
 
-          <div className="programs-section curve-part">
-            {programsData.map((item, index) => (
+        {/* Bootstrap Row */}
+        <div className="row programs-section">
+          {programsData.map((item, index) => (
+            <div
+              key={index}
+              className="col-lg-4 col-md-6 col-12 mb-4 d-flex"
+            >
               <ProgramCard
-                key={index}
                 image={item.image}
                 title={item.title}
                 description={item.description}
               />
-            ))}
-          </div>
-          
-          <div className="view-btn">
-            <Link to="/demo" >
-              <Button text="View all" variant="primary" />
-            </Link>
-          </div>
+            </div>
+          ))}
         </div>
-      </section>
-    </>
+
+        <div className="view-btn">
+          <Link to="/demo">
+            <Button text="View all" variant="primary" />
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
--- src/Component/WhyChooseUs/MiniCardSection/MiniCardSection.jsx
+++ src/Component/WhyChooseUs/MiniCardSection/MiniCardSection.jsx
@@ -1,34 +1,42 @@
-import React from 'react'
-import CmnHeading from '../../CmnHeading/CmnHeading'
-import { CardsData } from '../../../data/cardsData'
-import MiniCard from '../MiniCard'
+import React from "react";
+import CmnHeading from "../../CmnHeading/CmnHeading";
+import { CardsData } from "../../../data/cardsData";
+import MiniCard from "../MiniCard";
 
 import "./MiniCardSection.scss";
 import "../../../Main.scss";
 
 export default function MiniCardSection() {
-    return (
-        <>
-            <div className="why-choose-us">
-                <CmnHeading
-                    title="Why Choose Us"
-                    subtitle="Give the Gift of Gym"
-                    details="Lorem ipsum dolor sit amet consectetur. Vitae elit quam volutpat id. Quisque orci lacinia sit non. Diam et adipiscing proin orci. Eget lorem sit etiam molestie rhoncus non. Ut tincidunt tristique suspendisse arcu ac."
-                    align="center"
-                />
-            </div>
-            <div className="cards-section">
+  return (
+    <section className="mini-section">
+      <div className="container">
 
-                {CardsData.map((item, index) => (
-                    <MiniCard
-                        key={index}
-                        icon={item.icon}
-                        title={item.title}
-                        description={item.description}
-                        color={item.color}
-                    />
-                ))}
+        <div className="why-choose-us">
+          <CmnHeading
+            title="Why Choose Us"
+            subtitle="Give the Gift of Gym"
+            details="Lorem ipsum dolor sit amet consectetur. Vitae elit quam volutpat id. Quisque orci lacinia sit non. Diam et adipiscing proin orci. Eget lorem sit etiam molestie rhoncus non. Ut tincidunt tristique suspendisse arcu ac."
+            align="center"
+          />
+        </div>
+
+        <div className="row cards-section g-4">
+          {CardsData.map((item, index) => (
+            <div
+              key={index}
+              className="col-lg-3 col-md-6 col-12"
+            >
+              <MiniCard
+                icon={item.icon}
+                title={item.title}
+                description={item.description}
+                color={item.color}
+              />
             </div>
-        </>
-    )
+          ))}
+        </div>
+
+      </div>
+    </section>
+  );
 }
