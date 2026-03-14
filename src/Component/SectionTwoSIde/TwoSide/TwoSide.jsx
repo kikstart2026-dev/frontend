f785aa9308228e8182b65973bf870e803816db81
kikstart2026-dev <kikstart2026@gmail.com>
2026-03-15 01:01:28 +0530

    about admin panel done

--- src/Component/SectionTwoSIde/TwoSide/TwoSide.jsx
+++ src/Component/SectionTwoSIde/TwoSide/TwoSide.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import MainTwoSide from "../MainTwoSide";
 import Button from "../../Buttons/Button";
-import CmnHeading from "../../CmnHeading/CmnHeading";
 import styles from "./TwoSide.module.scss";
 import { getAllAboutUs } from "../../../apis/api";
+import noImg from "../../../assets/images/no-img.png";
+import CmnHeading from "../../CmnHeading/CmnHeading";
+
 
 export default function TwoSide() {
 
@@ -24,7 +26,19 @@ export default function TwoSide() {
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Failed to load about section</p>;
-  if (!about) return null;
+
+  const displayedAbout = about || {
+    headingData: {
+      tagline: "Not Found",
+      heading: "No Heading",
+      description:
+        "Descrption is not available",
+    },
+    image: noImg,
+  };
+
+
+
 
   return (
     <div className="both-space">
@@ -32,31 +46,33 @@ export default function TwoSide() {
         <div className="row align-items-center">
 
           <div className="col-lg-6 col-md-6 col-12">
-            <MainTwoSide img={about.image} />
+            <MainTwoSide img={displayedAbout.image} />
           </div>
 
           <div className="col-lg-6 col-md-6 col-12">
 
             <CmnHeading
-              title={about.headingData?.tagline}
-              subtitle={about.headingData?.heading}
+              title={displayedAbout.headingData?.tagline}
+              subtitle={displayedAbout.headingData?.heading}
               details={
                 <>
                   <p className={styles.firstPara}>
-                    {about?.headingData?.description?.split("|")[0]}
+                    {displayedAbout?.headingData?.description?.split("|")[0]}
                   </p>
 
                   <p className={styles.secondPara}>
-                    {about?.headingData?.description?.split("|")[1]}
+                    {displayedAbout?.headingData?.description?.split("|")[1]}
                   </p>
                 </>
               }
               align="left"
             />
 
-            <div className={styles.buttonWrapper}>
-              <Button text="know more" variant="primary" />
-            </div>
+            {about && (
+              <div className={styles.buttonWrapper}>
+                <Button text="know more" variant="primary" />
+              </div>
+            )}
 
           </div>
 
