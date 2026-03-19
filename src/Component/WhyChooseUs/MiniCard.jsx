378d282bab77fb1fd80c6ca9eaad2601641454cd
kikstart2026-dev <kikstart2026@gmail.com>
2026-03-19 20:02:09 +0530

    ff

--- src/Component/WhyChooseUs/MiniCard.jsx
+++ src/Component/WhyChooseUs/MiniCard.jsx
@@ -13,7 +13,7 @@ export default function MiniCard({ icon, title, description, color }) {
       </figure>
 
       <h3>{title}</h3>
-      <p>{description}</p>
+      <p dangerouslySetInnerHTML={{__html: {description},}}></p>
     </div>
   );
 }
