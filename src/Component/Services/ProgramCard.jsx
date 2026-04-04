8b49623289a73cded2d91ccbb5424897642c408c
kikstart2026-dev <kikstart2026@gmail.com>
2026-04-04 23:15:44 +0530

    jj

--- src/Component/Services/ProgramCard.jsx
+++ src/Component/Services/ProgramCard.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import styles from "./ProgramCard.module.scss";
+import { Link } from "react-router-dom";
 
-export default function ProgramCard({ image, title, description }) {
+export default function ProgramCard({ image, title, description, details2, video }) {
   return (
-    <div className={styles["program-card"]}>
-      <figure>
-        <img src={image} alt={title} />
-      </figure>
+    <Link
+      to="/ProgramDeatailsPage"
+      state={{ image, title, description, details2, video }}
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <div className={styles["program-card"]}>
+        <figure>
+          <img src={image} alt={title} />
+        </figure>
 
-      <div className={styles.overlay}>
-        <h3>{title}</h3>
-        <p
-          dangerouslySetInnerHTML={{ __html: description }}
-        ></p>
+        <div className={styles.overlay}>
+          <h3>{title}</h3>
+          <p dangerouslySetInnerHTML={{ __html: description }}></p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
