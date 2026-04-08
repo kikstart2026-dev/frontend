0d1341b3d1c734916c251032334b9d76954d1086
kikstart2026-dev <kikstart2026@gmail.com>
2026-04-08 22:02:30 +0530

    ...

--- src/Component/AboutMid/AboutMid.jsx
+++ src/Component/AboutMid/AboutMid.jsx
@@ -1,28 +1,37 @@
 import React from 'react'
-import man from '../../assets/images/man.png'
 import MainTwoSide from '../SectionTwoSIde/MainTwoSide'
 import CmnHeading from '../CmnHeading/CmnHeading'
 import styles from './AboutMid.module.scss'
 
-export default function AboutMid() {
+export default function AboutMid({ aboutData }) {
+
+  const desc = aboutData?.headingData?.description || "";
+  const parts = desc.split("|");
+
   return (
     <div className={styles.aboutMid}>
       <div className="row align-items-center">
-        
+
         <div className="col-lg-6 col-md-6 col-12">
-          
+
           <CmnHeading
-            title="about us"
-            subtitle="Who we are"
+            title={aboutData?.headingData?.tagline || "About Us"}
+            subtitle={aboutData?.headingData?.heading || "Who we are"}
             details={
               <>
-                <p className={styles.firstPara}>
-                  Lorem ipsum dolor sit amet consectetur. Vitae elit quam volutpat id. Quisque orci lacinia sit non. Diam et adipiscing proin orci. Eget lorem sit etiam molestie rhoncus non. Ut tincidunt tristique suspendisse arcu ac.Curabitur suspendisse tellus placerat libero ut. Enim auctor velit massa integer. Amet interdum at vivamus aliquet mattis integer magna aliquam.Nulla urna aliquam sit eget ac dolor aliquam tincidunt.
-                </p>
+                <p
+                  className={styles.firstPara}
+                  dangerouslySetInnerHTML={{
+                    __html: parts[0] || "",
+                  }}
+                />
 
-                <p className={styles.secondPara}>
-                  Ut fermentum elementum amet elementum arcu suspendisse. Vitae lectus penatibus est sit iaculis quis. Auctor eu vitae imperdiet dignissim hendrerit. A elementum turpis sem quis. Ut tincidunt tristique suspendisse arcu ac.Curabitur suspendisse tellus placerat libero ut.
-                </p>
+                <p
+                  className={styles.secondPara}
+                  dangerouslySetInnerHTML={{
+                    __html: parts[1] || "",
+                  }}
+                />
               </>
             }
             align="left"
@@ -30,7 +39,7 @@ export default function AboutMid() {
         </div>
 
         <div className="col-lg-6 col-md-6 col-12 space3">
-          <MainTwoSide img={man} />
+          <MainTwoSide img={aboutData?.image} />
         </div>
 
       </div>
