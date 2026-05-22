92ffe618a61b77b1c4a031f8489f4cec49d7452e
kikstart2026-dev <kikstart2026@gmail.com>
2026-05-22 14:09:00 +0530

    ----

--- src/Shared/DashboardHeader/DashboardHeader.jsx
+++ src/Shared/DashboardHeader/DashboardHeader.jsx
@@ -1,42 +1,61 @@
 import React, { useEffect, useState } from "react";
-
+import kiklogo from "../../assets/images/authLogo.png";
+import { NavLink, useLocation } from "react-router-dom";
 import styles from "./DashboardHeader.module.scss";
 
-export default function DashboardHeader({
-  title = "Dashboard",
-}) {
+export default function DashboardHeader() {
   const [admin, setAdmin] = useState(null);
+  const location = useLocation();
 
   useEffect(() => {
-    const storedAdmin =
-      localStorage.getItem("user");
+    const storedAdmin = localStorage.getItem("user");
 
     if (storedAdmin) {
       setAdmin(JSON.parse(storedAdmin));
     }
   }, []);
 
+  // ✅ ROUTE BASED TITLE
+  const getTitle = () => {
+    switch (location.pathname) {
+      case "/dashboard":
+        return "Dashboard";
+      case "/dashboard/messages":
+        return "Message";
+      case "/dashboard/transactions":
+        return "My Transactions";
+      case "/dashboard/programs":
+        return "Programs";
+      case "/dashboard/children-profile":
+        return "Children Profile";
+      default:
+        return "Dashboard";
+    }
+  };
+
   return (
     <div className={styles.topbar}>
-      <h2>{title}</h2>
+      {/* LEFT LOGO */}
+      <div className="d-flex align-items-center">
+        <NavLink to="/">
+          <img src={kiklogo} alt="logo" className={styles.logo} />
+        </NavLink>
+      </div>
 
+      {/* TITLE */}
+      <h2>{getTitle()}</h2>
+
+      {/* USER */}
       <div className={styles.userBox}>
         {admin?.user?.image ? (
-          <img
-            src={admin.user.image}
-            alt="user"
-            className={styles.avatar}
-          />
+          <img src={admin.user.image} alt="user" className={styles.avatar} />
         ) : (
           <div className={styles.fallbackAvatar}>
             {admin?.fullname?.charAt(0) || "U"}
           </div>
         )}
 
-        <span>
-          Welcome,{" "}
-          {admin?.fullname || "User"}
-        </span>
+        <span>Welcome, {admin?.fullname || "User"}</span>
       </div>
     </div>
   );
