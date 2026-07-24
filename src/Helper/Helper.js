2cd053e6173c420d50abd7cd0baddf5f2620bc3d
kikstart2026-dev <poulomisaha588@gmail.com>
2026-07-24 15:01:49 +0530

    --

--- src/Helper/Helper.js
+++ src/Helper/Helper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8008/api/v1",
+  baseURL: "https://backend-8e6g.onrender.com/api/v1",
 });
 
 axiosInstance.interceptors.request.use(
